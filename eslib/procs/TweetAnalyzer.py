6dc5d7aa1c9531b46fbbabdd4ab6c3857f164367
Nets user DEV <amazon.ec2.nets@comperiosearch.com>
2014-03-28 09:53:42 +0000

    Added option for verbose debug info

--- eslib/procs/TweetAnalyzer.py
+++ eslib/procs/TweetAnalyzer.py
@@ -184,6 +184,7 @@ def main():
     parser.add_argument("-t", "--targets" , help=help_t, required=True , metavar="file")
     parser.add_argument("-f", "--field"   , help=help_f, required=False, metavar="field", default="text")
     parser.add_argument(      "--debug"   , help="Display debug info." , action="store_true")
+    parser.add_argument(      "--verbose"   , help="Display verbose debug info." , action="store_true")
     parser.add_argument("filenames", nargs="*", help="If not specified stdin will be used instead.")
 
     if len(sys.argv) == 1:
@@ -200,6 +201,7 @@ def main():
     dp.field        = args.field
 
     dp.DEBUG = args.debug
+    dp.VERBOSE = args.verbose
 
     dp.run(args.filenames)
 
