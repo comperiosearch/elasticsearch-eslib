e88d03ebf1ea0ca7e92e9ecf714e60b908f845d7
Mats Julian Olsen <mats@plysjbyen.net>
2014-10-10 13:43:51 +0200

    Fix stupid merge

--- eslib/procs/__init__.py
+++ eslib/procs/__init__.py
@@ -7,7 +7,7 @@ eslib.procs
 Document processing processors.
 """
 
-<<<<<<< HEAD
+
 from .ElasticsearchReader   import ElasticsearchReader
 from .ElasticsearchWriter   import ElasticsearchWriter
 from .FileReader            import FileReader
@@ -20,21 +20,7 @@ from .Neo4jWriter           import Neo4jWriter
 from .Neo4jReader           import Neo4jReader
 from .TwitterUserGetter     import TwitterUserGetter
 from .TwitterFollowerGetter import TwitterFollowerGetter
-=======
-from .ElasticsearchReader import ElasticsearchReader
-from .ElasticsearchWriter import ElasticsearchWriter
-from .FileReader          import FileReader
-from .FileWriter          import FileWriter
-from .RabbitmqMonitor     import RabbitmqMonitor
-from .RabbitmqWriter      import RabbitmqWriter
-from .HttpMonitor         import HttpMonitor
-#from .TwitterMonitor      import TwitterMonitor
-from .CsvConverter        import CsvConverter
-from .WebGetter           import WebGetter
-from .Neo4jWriter         import Neo4jWriter
-from .Neo4jReader         import Neo4jReader
-#from .TwitterUserGetter   import TwitterUserGetter
->>>>>>> b32100a60ee12760f29f449584a4bed3b8682f16
+
 
 __all__ = (
     "ElasticsearchReader",
