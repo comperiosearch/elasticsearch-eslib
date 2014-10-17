289695abc314d3d0e5d2eff259a809278c6f56ad
Hans Terje Bakke <hans.terje.bakke@gmail.com>
2014-10-17 12:48:20 +0200

    Fixed stdin and stdout encoding in prog

--- eslib/prog.py
+++ eslib/prog.py
@@ -11,7 +11,13 @@ Helper functions for running as an executable program.
 __all__ = ( "progname", "initlogs")
 
 
-import os, sys, logging.config, yaml
+import os, sys, codecs, logging.config, yaml
+
+# Fix stdin and stdout encoding issues
+_encoding_stdin  = sys.stdin.encoding or "UTF-8"
+_encoding_stdout = sys.stdout.encoding or _encoding_stdin
+sys.stdin = codecs.getreader(_encoding_stdin)(sys.stdin)
+sys.stdout = codecs.getwriter(_encoding_stdout)(sys.stdout)
 
 def progname():
     return os.path.basename(sys.argv[0])
