0df8d541ab96e3fa83a022a56f679d07a347d83d
Eivind Eidheim Elseth <eivind.elseth@comperiosearch.com>
2014-10-31 13:55:43 +0100

    Added a basic entity extractor with tests and example
    This initial commit only has exact matches, but as per HTBs wishes I have
    tried to keep the format open so that we can easily add more match types
    later.
    
    I'm sure this code needs some reviewing, I won't take it personal.

---
+++ eslib/procs/EntityExtractor.py
@@ -0,0 +1,60 @@
+__author__ = 'Eivind Eidheim Elseth'
+
+from ..Processor import Processor
+from .. import esdoc
+
+
+class EntityExtractor(Processor):
+
+    def __init__(self, **kwargs):
+        super(EntityExtractor, self).__init__(**kwargs)
+        self.create_connector(self._incoming_esdoc, "input", "esdoc", "Incoming 'esdoc'.", is_default=True)
+#        self.create_connector(self._incoming_str  , "str"  , "str"  , "Incoming document of type 'str' or 'unicode'.")
+        self.output_esdoc = self.create_socket("output" , "esdoc"   , "Outgoing 'esdoc' with.", is_default=True)
+#        self.output_str   = self.create_socket("str"    , "str"     , "Outgoing, cleaned, 'str'.")
+        self.config.set_default(
+            entities = [],
+            source_field = "text",
+            target_field = "entities"
+        )
+        self._entities = []
+
+    def on_open(self):
+        self._entities = self.config.entities
+        pass
+
+    def _incoming_esdoc(self, doc):
+        if self.output_esdoc.has_output:
+            self.output_esdoc.send(self._extract(doc))
+
+    def _extract(self, doc):
+        source = doc.get("_source")
+        text = esdoc.getfield(source, self.config.source_field)
+        extracted = self._extract_exact(text)
+        return esdoc.shallowputfield(doc, "_source." + self.config.target_field, extracted)
+
+
+
+    def addEntity(self, entity):
+        self._entities.append(entity)
+
+    def addEntities(self, entities):
+        self._entities.extend(entities)
+
+    def _extract_exact(self, input):
+        extracted = {}
+        for entity in self._entities:
+            for match in entity["match"]:
+                if match["type"] == "exact":
+                    if match["value"] in input:
+                        if not entity["category"] in extracted:
+                            extracted[entity["category"]] = []
+                        extracted[entity["category"]].extend([
+                            {
+                                entity["name"] : {
+                                    match["type"] : [match["value"]]
+                                }
+                            }
+                        ])
+
+        return extracted
---
+++ test/procs/test_entity_extractor.py
@@ -0,0 +1,95 @@
+__author__ = 'Eivind Eidheim Elseth'
+
+import unittest, json
+from eslib.procs.EntityExtractor import EntityExtractor
+
+class TestEntityExtractor(unittest.TestCase):
+    entities = \
+        [
+            {
+                "category": "webpage",
+                "name": "nrk.no",
+                "match": [
+                    { "type": "exact", "value": "nrk" },
+                    #{ "type": "iprange", "value": "160.68.205.231/16" }
+                ]
+            },
+            {
+                "category": "agent",
+                "name": "18036928545",
+                "match": [
+                    { "type": "exact", "value": "hans terje bakke" },
+                ]
+            }
+        ]
+    entity = {
+                "category": "webpage",
+                "name": "comperio",
+                "match": [
+                    { "type": "exact", "value": "comperio" },
+                    #{ "type": "iprange", "value": "81.27.32.186/16" }
+                ]
+            }
+
+
+    def setUp(self):
+        self.extractor = EntityExtractor()
+        self.extractor.on_open()
+
+
+
+    def test_defaults(self):
+        self.assertEqual(self.extractor.config.source_field, "text")
+        self.assertEqual(self.extractor.config.target_field, "entities")
+        self.assertEqual(self.extractor.config.entities, [])
+        pass
+
+    def test_add_entities(self):
+        self.assertEqual(self.extractor._entities, [])
+        self.extractor.addEntity(self.entity)
+        expected = [self.entity]
+        actual = self.extractor._entities
+        self.assertEqual(expected, actual)
+
+        self.extractor.addEntities(self.entities)
+
+        expected.extend(self.entities)
+        actual = self.extractor._entities
+        self.assertEqual(expected, actual)
+
+    def test_extract_exact(self):
+        self.extractor.addEntity(self.entity)
+        comperio = self.entity
+        match_comperio = comperio["match"][0]
+        input = "We all love " + match_comperio["value"]
+        expected = {}
+        expected[comperio["category"]] = [self._create_result_object(comperio, match_comperio)]
+        actual = self.extractor._extract_exact(input)
+        self.assertEqual(expected, actual)
+
+        nrk = self.entities[0]
+        self.extractor.addEntity(nrk)
+        match_nrk = nrk["match"][0]
+        expected[nrk["category"]].append(self._create_result_object(nrk,match_nrk))
+        input = "we at" + match_comperio["value"] + " love watching " + match_nrk["value"]
+        actual = self.extractor._extract_exact(input)
+        self.assertEqual(expected, actual)
+
+        hans_terje = self.entities[1]
+        self.extractor.addEntity(hans_terje)
+        match_hans_terje = hans_terje["match"][0]
+        expected[hans_terje["category"]] = [self._create_result_object(hans_terje,match_hans_terje)]
+        input =  match_hans_terje["value"] + "works at " + match_comperio["value"] + "and  loves watching " + match_nrk["value"]
+        actual = self.extractor._extract_exact(input)
+        self.assertEqual(expected, actual)
+
+
+    def _create_result_object(self, entity, match):
+        return {
+                entity["name"] : {
+                    match["type"]: [
+                        match["value"]
+                    ]
+                }
+            }
+
