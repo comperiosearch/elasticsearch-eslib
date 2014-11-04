e14c8b0d2bf08fc132c83393000ffc2bafa0cc45
Hans Terje Bakke <hans.terje.bakke@gmail.com>
2014-11-04 12:49:10 +0100

    ProtocolConverter test function that would replace ParseEdgeToIds

--- test/procs/test_protocol_converter.py
+++ test/procs/test_protocol_converter.py
@@ -14,7 +14,6 @@ class TestProtocolConverter(unittest.TestCase):
         csv2list = lambda doc: [",".join(doc)]
 
         p = ProtocolConverter(func=csv2list, input_protocol="list", output_protocol="csv")
-        p.keepalive = True
 
         output = []
         p.add_callback(lambda doc: output.append(doc))
@@ -36,7 +35,6 @@ class TestProtocolConverter(unittest.TestCase):
     def test_func_multi_yield(self):
 
         p = ProtocolConverter(func=self.yieldfunc, input_protocol="str", output_protocol="str")
-        p.keepalive = True
 
         output = []
         p.add_callback(lambda doc: output.append(doc))
@@ -52,6 +50,34 @@ class TestProtocolConverter(unittest.TestCase):
         print "output=", joined
 
         self.assertEqual(joined, "a,A,b,B,c,C")
+
+
+    def edge2ids(self, doc):
+        if doc["type"] == "author":
+            yield doc["from"]
+        else:
+            yield doc["from"]
+            yield doc["to"]
+
+    def test_graph_edge_convertion(self):
+        p = ProtocolConverter(func=self.edge2ids, input_protocol="str", output_protocol="str")
+
+        output = []
+        p.add_callback(lambda doc: output.append(doc))
+
+        p.start()
+        p.put({"type": "author" , "from": "1", "to": "1"})
+        p.put({"type": "mention", "from": "2", "to": "3"})
+        p.put({"type": "quote"  , "from": "4", "to": "1"})
+        p.stop()
+        p.wait()
+
+        joined = ",".join(output)
+        print "output=", joined
+
+        self.assertEqual(joined, "1,2,3,4,1")
+
+
 
 def main():
     unittest.main()
