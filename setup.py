051ec07f0968f5fec2e9303ed808581a21644933
Hans Terje Bakke <hans.terje.bakke@gmail.com>
2016-02-11 15:22:48 +0100

    Upped version to .10 for pypi dist

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ requires = [
 
 setup(
     name='eslib',
-    version='0.0.9',
+    version='0.0.10',
     description='Document processing framework and utility for Elasticsearch (or whatever).',
     #long_description=open("README.md").read(),
     author='Hans Terje Bakke',
